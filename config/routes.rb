b8202b8aca041c54940545470d660bbba22a1ced
audrone cirkelyte <audracirk@gmail.com>
2023-02-11 17:51:11 +0100

    watch list & bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new", as: :new_list
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: :list
+  resources :lists, only: [:index, :new, :create, :show, :destroy] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
