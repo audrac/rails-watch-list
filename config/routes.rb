944a9981d34462f3771360712b3e0e2a24444738
audrone cirkelyte <audracirk@gmail.com>
2023-02-12 22:20:55 +0100

    changed routes for roots

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
   # get "lists", to: "lists#index"
   # get "lists/new", to: "lists#new", as: :new_list
   # post "lists", to: "lists#create"
