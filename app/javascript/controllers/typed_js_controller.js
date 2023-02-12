d5af70d7f420303bc24a3647bb7d0b651bd18ed2
audrone cirkelyte <audracirk@gmail.com>
2023-02-12 21:40:29 +0100

    uploaded images

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["Only those that you really like"],
+      typeSpeed: 50,
+      loop: false
+    })
+  }
+}
